592efb8972dae4126d36d6f34b0e2ba3cdb720df
Creon228 <ivank2977@gmail.com>
2021-11-29 12:54:17 +0300

    Исправлены ошибки

--- PZ_5/PZ_5_2.py
+++ PZ_5/PZ_5_2.py
@@ -1,11 +1,19 @@
 
+from math import sqrt, pow
 
+def RectPS():
+    a = sqrt(pow((x1 - x1),2) + pow((y1 - y2),2))
+    b = sqrt(pow((x1 - x2),2) + pow((y1 - y1),2))
+    S = a * b
+    P = (a + b) *2
 
+    print("Площадь = ", S)
+    print("Периметр = ", P)
 
+x1 = int(input("введите x1: "))
+y1 = int(input("введите y1: "))
+x2 = int(input("введите x2: "))
+y2 = int(input("введите y2: "))
 
-
-
-
-
-
+RectPS()
 
